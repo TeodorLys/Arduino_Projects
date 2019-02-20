b5f131093c5f35c622bbb5c61eb33f0e604fde85
Teodor Lysvret van Ginhoven <teodorvanginhoven@gmail.com>
2019-02-20 16:18:05 +0100

    Add files via upload

---
+++ LED_Panel.ino
@@ -0,0 +1,22 @@
+#include <LedControl.h>
+#include "Letters.h"
+#include "Letter_Array.h";
+Letter_Array la;
+
+unsigned long time;
+
+int count = 0;
+void setup() {
+  //Serial.begin(9600);
+  la.Init_Panels();
+  la.set_String("teodor van ginhoven");
+  time = millis();
+}
+
+void loop() {
+   la.draw();
+
+  delay(5);
+  la.Move_Left(1);
+  //delay(5);
+}
